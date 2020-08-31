0e9ffbd43669f1e7787b946aea1ab696a1b8d79c
annshress <ann.shress@gmail.com>
2020-08-31 09:16:51 +0545

    Fix: missing field

--- server/apps/entry/tests/test_apis.py
+++ server/apps/entry/tests/test_apis.py
@@ -59,6 +59,7 @@ class TestFigureCreation(HelixGraphQLTestCase):
             ],
             "includeIdu": False,
             "entry": self.entry.id,
+            "startDate": "2020-10-10",
         }
         self.force_login(self.creator)
 
@@ -71,6 +72,7 @@ class TestFigureCreation(HelixGraphQLTestCase):
         )
         content = json.loads(response.content)
 
+        print(content)
         self.assertResponseNoErrors(response)
         self.assertFalse(content['data']['createFigure']['ok'], content)
         self.assertIn('non_field_errors',
